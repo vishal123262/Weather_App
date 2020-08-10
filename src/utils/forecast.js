61d42e78554b0819d9e8a006ad9d60cfec705fff
Vishal123262 <vishalmodani8@gmail.com>
2020-08-10 12:35:13 +0530

    Made changes in weather description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if ( body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,'It is currently ' +  body.current.temperature + ' degress out. There is a ' +  body.current.precip + '% chance of rain.')
-        }                                                                                                                                                                                               
+            callback(undefined,body.current.weather_descriptions[0] +'.It is currently ' + 
+            body.current.temperature + ' degress out. There is a ' +  
+            body.current.precip + '% chance of rain.'+'The humidity is '+body.current.humidity+'%')
+        }   
+
     })
 }
 
